2d439ea4ac90472a8db60dd74d916cb6b04d433b
William Bergekrans <wilbe251@student.liu.se>
2022-02-17 16:37:52 +0100

    navbar and global theme added

---
+++ playing-cards/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
+};
+
+export default App;
--- playing-cards/src/index.js
+++ playing-cards/src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { green, lime } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: lime[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hellooooo World!</h1>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
